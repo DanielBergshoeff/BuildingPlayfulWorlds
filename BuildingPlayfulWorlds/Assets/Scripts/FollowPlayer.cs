3b5ec2b00b7d48f369b31ff18688022715f10838
unknown <dabergshoeff@gmail.com>
2018-02-16 12:11:06 +0100

    Added shooting with Raycast - added crosshair - added enemies following player - added enemies becoming multiple small ones once shot

---
+++ BuildingPlayfulWorlds/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public int moveSpeed = 3;
+    public int rotationSpeed = 3;
+    public int range = 10;
+    public int range2 = 10;
+
+
+    private Transform myTransform;
+    private Transform target;
+
+    private void Awake()
+    {
+        myTransform = transform;
+    }
+
+    // Use this for initialization
+    void Start () {
+        target = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var distance = Vector3.Distance(myTransform.position, target.position);
+        if (distance < range)
+        {
+            myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+            myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+}
---
+++ BuildingPlayfulWorlds/Assets/Scripts/Shooting.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+
+    public GameObject prefab;
+
+    public Texture2D crosshairImage;
+
+    public int range;
+
+
+
+    private Camera fpsCam;
+
+    private void OnGUI()
+    {
+        float xMin = (Screen.width / 2) - (crosshairImage.width / 2);
+        float yMin = (Screen.height / 2) - (crosshairImage.height / 2);
+        GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
+    }
+
+    // Use this for initialization
+    void Start () {
+        fpsCam = GameObject.Find("FirstPersonCharacter").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")){
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(.5f, .5f, 0));
+            RaycastHit hit;
+            if(Physics.Raycast (rayOrigin, fpsCam.transform.forward, out hit, range))
+            {
+                if(hit.collider.gameObject.tag == "Enemy")
+                {
+                    Vector3 tempPosition = hit.collider.transform.position; 
+                    Quaternion tempRotation = hit.collider.transform.rotation;                    
+                    Destroy(hit.collider.gameObject);
+                    Instantiate(prefab, tempPosition, tempRotation);
+                }
+                else if(hit.collider.gameObject.tag == "SmallEnemy")
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+	}
+}
